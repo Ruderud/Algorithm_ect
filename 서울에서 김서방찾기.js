c2955b7af4a13087619437bdb3302e10ff399219
Ruderud <jungkh05@naver.com>
2021-08-06 18:07:19 +0900

    indexOf, findIndex 메서드 사용

--- 서울에서 김서방찾기.js
+++ 서울에서 김서방찾기.js
@@ -1,1 +1,9 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12919
+
+function solution(seoul) {
+  let site = seoul.findIndex((val) => val === "Kim");
+
+  return `김서방은 ${site}에 있다`;
+}
+
+//indexOf도 쓸수있음 => seoul.indexOf("Kim") // 1
